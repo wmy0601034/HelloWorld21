54fc480f1df90fb8edcb5ac5990c9c04e3cf07fa
wumingyue <wmy0601034@163.com>
2018-09-22 00:47:12 +0800

    this is a test

--- app/src/main/java/com/nanningzhuanqian/helloworld/MainActivity.java
+++ app/src/main/java/com/nanningzhuanqian/helloworld/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+//        Successfully created project 'HelloWorld2' on GitHub, but initial commit failed: *** Please tell me who you are. Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. fatal: empty ident name (for  ) no
     }
 }
